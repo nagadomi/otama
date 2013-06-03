5c785c191dd09ccca3bb1663c95e6a33506519c8
nagadomi <nagadomi@nurs.or.jp>
2013-06-03 15:44:32 +0900

    LMCAの学習プログラムにvalidation機能を追加

--- src/nvlmcaex/nv_lmca_train.cpp
+++ src/nvlmcaex/nv_lmca_train.cpp
@@ -41,39 +41,161 @@ print_usage(void)
 	printf("%s [OPTIONS] [filelist.txt]\n"
 		   "    -e (vlad|hsv|vladhsv) extract mode\n"
 		   "    -t (vlad|hsv|vladhsv|vq) training mode\n"
-		   "    -q s   path to the vlad vector quantization file(i/o)\n"
-		   "    -f s   path to the data file(i/o)\n"
+		   "    -v (vlad|hsv|vladhsv) validation mode\n"
 		   " extract mode options\n"
-		   "    -x     use flip image\n"
+		   "    -x     use flip image (2x data)\n"
+		   "    -q s   path to the codebook_file(input)\n"
+		   "    -f s   path to the data_file (output)\n"
 		   " vlad/hsv/vladhsv training mode options\n"
 		   "    -i n   iteration (default: 50)\n"
 		   "    -k n   pull kNN k (default: 20)\n"
-		   "    -n n   push kNN k (default: 32)\n"
+		   "    -n n   push kNN k (default: k + k / 2 + 2)\n"
 		   "    -r f   push weight (default: vlad: 0.3, vladhsv: 0.3, hsv: 0.45, pull weight: 1.0-f)\n"
 		   "    -m f   margin (default: 0.05)\n"
 		   "    -d f   learning rate (default: 0.1)\n"
+		   "    -q s   path to the codebook_file (input)\n"
+		   "    -f s   path to the data_file (input)\n"
 		   "    -o s   path to the lmca_file (output)\n"
 		   "    -l s   path to the lmca_file (resume input)\n"
 		   " vq training mode options\n"
-		   "    -n n   max keypoints (default: 10000000)\n",
+		   "    -q s   path to the codebook_file(output)\n"
+		   "    -n n   max keypoints (default: 10000000)\n"
+		   " validation mode options\n"
+		   "    -k n   kNN closet size (default: 20)\n"
+		   "    -l s   path to the lmca_file (input)\n"
+		   "    -f s   path to the data_file(input)\n",
 		   name
 		);
+}
+
+static void
+knn_validation(const nv_matrix_t *l,
+			   const nv_matrix_t *data,
+			   const nv_matrix_t *labels,
+			   int k)
+{
+	nv_matrix_t *data_lmca = nv_matrix_alloc(l->m, data->m);
+	int i;
+	int max_class1 = 0;
+	int *count;
+	int *knn_correct;
+	int *true_positive;
+	
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif	
+	for (i = 0; i < data->m; ++i) {
+		nv_lmca_projection(data_lmca, i, l, data, i);
+		nv_vector_normalize(data_lmca, i);
+	}
+	
+	for (i = 0; i < labels->m; ++i) {
+		max_class1 = NV_MAX(max_class1, NV_MAT_VI(labels, i, 0));
+	}
+	max_class1 += 1;
+	
+	count = nv_alloc_type(int, max_class1);
+	knn_correct = nv_alloc_type(int, max_class1);
+	true_positive = nv_alloc_type(int, max_class1);
+	memset(count, 0, sizeof(int) * max_class1);
+	memset(knn_correct, 0, sizeof(int) * max_class1);
+	memset(true_positive, 0, sizeof(int) * max_class1);
+	
+	for (i = 0; i < labels->m; ++i) {
+		
+	}
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+	for (i = 0; i < data->m; ++i) {
+		int j, n;
+		int true_label = NV_MAT_VI(labels, i, 0);
+		int max_count;
+		int max_index = 0;
+		nv_knn_result_t *results = nv_alloc_type(nv_knn_result_t, k);
+		int *label_count = nv_alloc_type(int, max_class1);
+		
+		memset(label_count, 0, sizeof(int) * max_class1);
+		n = nv_knn(results, k, data_lmca, data_lmca, i);
+		for (j = 0; j < n; ++j) {
+			int label = NV_MAT_VI(labels, results[j].index, 0);
+			if (label == true_label) {
+#ifdef _OPENMP
+#pragma omp atomic
+#endif
+				++true_positive[true_label];
+				
+			}
+			if (results[j].index != i) {
+#ifdef _OPENMP
+#pragma omp atomic
+#endif
+				++label_count[label];
+			}
+		}
+		max_count = -1;
+		for (j = 0; j < max_class1; ++j) {
+			if (max_count < label_count[j]) {
+				max_count = label_count[j];
+				max_index = j;
+			}
+		}
+		if (max_index == true_label) {
+#ifdef _OPENMP
+#pragma omp atomic
+#endif
+			++knn_correct[true_label];
+		}
+#ifdef _OPENMP
+#pragma omp atomic
+#endif
+		++count[true_label];
+		
+		nv_free(label_count);
+		nv_free(results);
+	}
+	{
+		float ap = 0.0f;
+		float ar = 0.0f;
+		float aa = 0.0f;
+		printf("Search Results = %d\n", k);
+		for (i = 0; i < max_class1; ++i) {
+			float atp = (float)true_positive[i] / count[i];
+			float precision = 1.0f / k * atp;
+			float recall = 1.0f / count[i] * atp;
+			float accuracy = (float)knn_correct[i] / count[i];
+			const char *mark = accuracy > 0.0f ? "":"*";
+			printf("Class %5d: Precision: %f, Recall: %f, kNN Accuracy: %f %s\n", 
+				   i, precision, recall, accuracy, mark);
+
+			ap += precision;
+			ar += recall;
+			aa += accuracy;
+		}
+		printf("----\nAverage    : Precision: %f, Recall: %f, kNN Accuracy: %f\n",
+			   ap / max_class1, ar / max_class1, aa / max_class1);
+	}
+	nv_free(count);
+	nv_free(true_positive);
+	nv_free(knn_correct);
+	nv_matrix_free(&data_lmca);
 }
 
 typedef enum {
 	UNKNOWN = 0,
 	TRAIN = 1,
-	EXTRACT = 2
+	EXTRACT = 2,
+	VALIDATION = 3
 } mode_e;
 
-template<nv_lmca_feature_e T, typename C>
-int lmca_main(mode_e mode, const char *vq_file,
-			  int k, int k_n,
-			  float margin, float push_weight, float learning_rate, int  max_epoch,
-			  const char *filelist, bool flip,
-			  const char *base_file,
-			  const char *_data_file,
-			  const char *_metric_file
+template<nv_lmca_feature_e T, typename C> int
+lmca_main(mode_e mode, const char *vq_file,
+		  int k, int k_n,
+		  float margin, float push_weight, float learning_rate, int  max_epoch,
+		  const char *filelist, bool flip,
+		  const char *base_file,
+		  const char *_data_file,
+		  const char *_metric_file
 	)
 {
 	nv_matrix_t *data = NULL;
@@ -127,7 +249,7 @@ int lmca_main(mode_e mode, const char *vq_file,
 		if (!(l->n == nv_lmca_ctx<T, C>::RAW_DIM
 			  && l->m == nv_lmca_ctx<T, C>::LMCA_DIM))
 		{
-			fprintf(stderr, "%s: invalid matrix\n", base_file);
+			fprintf(stderr, "%s: invalid matrix size\n", base_file);
 			return -1;
 		}
 	}
@@ -137,6 +259,7 @@ int lmca_main(mode_e mode, const char *vq_file,
 			return -1;
 		}
 	}
+	
 	switch (mode) {
 	case TRAIN:
 	{
@@ -150,7 +273,7 @@ int lmca_main(mode_e mode, const char *vq_file,
 		}
 		
 		if (nv_load_matrix_array_bin(data_file, mats, &len) != 0 || len != 2) {
-			fprintf(stderr, "%s: invalid data file\n", data_file);
+			fprintf(stderr, "%s: invalid matrix size\n", data_file);
 			return -1;
 		}
 		data = mats[0];
@@ -159,7 +282,7 @@ int lmca_main(mode_e mode, const char *vq_file,
 		{
 			nv_matrix_free(&data);
 			nv_matrix_free(&labels);
-			fprintf(stderr, "%s: invalid data file\n", data_file);
+			fprintf(stderr, "%s: invalid matrix size\n", data_file);
 			return -1;
 		}
 		
@@ -182,10 +305,42 @@ int lmca_main(mode_e mode, const char *vq_file,
 		mats[0] = data;
 		mats[1] = labels;
 		if (nv_save_matrix_array_bin(data_file, mats, 2) != 0) {
-			fprintf(stderr, "%s: error\n", data_file);
+			fprintf(stderr, "%s: io error\n", data_file);
 			nv_matrix_free(&data);
 			nv_matrix_free(&labels);
 		}
+		break;
+	}
+	case VALIDATION:
+	{
+		nv_matrix_t *mats[2];
+		int len = 2;
+		
+		if (l == NULL) {
+			fprintf(stderr, "error: missing lmca file (-l)\n");
+			return -1;
+		}
+		if (nv_load_matrix_array_bin(data_file, mats, &len) != 0 || len != 2) {
+			fprintf(stderr, "%s: invalid matrix size\n", data_file);
+			return -1;
+		}
+		data = mats[0];
+		labels = mats[1];
+		if (data->m != labels->m || data->n != nv_lmca_ctx<T, C>::RAW_DIM || labels->n != 1)
+		{
+			nv_matrix_free(&data);
+			nv_matrix_free(&labels);
+			fprintf(stderr, "%s: invalid data file\n", data_file);
+			return -1;
+		}
+		
+		k = NV_MIN(data->m, k);
+		k_n = NV_MIN(data->m, k_n);
+		
+		knn_validation(l, data, labels, k);
+		nv_matrix_free(&data);
+		nv_matrix_free(&labels);
+		nv_matrix_free(&l);
 		break;
 	}
 	default:
@@ -225,7 +380,6 @@ vq(int n, const char *file, bool flip, const char *vq_file)
 	return ret;
 }
 
-
 int
 main(int argc, char** argv)
 {
@@ -245,7 +399,7 @@ main(int argc, char** argv)
 	char metric_file[8192] = {0};
 	bool flip = false;
 	
-	while ((opt = nv_getopt(argc, argv, "e:t:hk:m:d:n:i:r:l:q:xf:o:")) != -1) {
+	while ((opt = nv_getopt(argc, argv, "e:t:hk:m:d:n:i:r:l:q:xf:o:v:")) != -1) {
 		switch (opt) {
 		case 'f':
 			strncpy(data_file, nv_getopt_optarg, sizeof(data_file) - 1);
@@ -283,6 +437,10 @@ main(int argc, char** argv)
 			break;
 		case 't':
 			mode = TRAIN;
+			strncpy(feature, nv_getopt_optarg, sizeof(feature) -1);
+			break;
+		case 'v':
+			mode = VALIDATION;
 			strncpy(feature, nv_getopt_optarg, sizeof(feature) -1);
 			break;
 		case 'q':
