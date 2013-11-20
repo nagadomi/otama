f82ec7ab8640d6b61465eec21340f393c28d58c2
nagadomi <nagadomi@nurs.or.jp>
2013-11-20 17:56:24 +0900

    ビルド周りの整理; makeからruby extensionもビルドされるようにした

--- ruby/test.sh
+++ ruby/test.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-ruby test.rb bovw2k.yaml && \
-ruby test.rb bovw8k.yaml && \
-ruby test.rb bovw512k.yaml && \
-ruby test.rb bovw512k_iv.yaml && \
-ruby test_similarity.rb bovw512k_nodb.yaml && \
-ruby test_similarity.rb vlad_nodb.yaml
+RUBY=ruby
+if test $# -gt 0; then
+    RUBY=$1
+fi
+$RUBY test.rb bovw2k.yaml && \
+$RUBY test.rb bovw8k.yaml && \
+$RUBY test.rb bovw512k.yaml && \
+$RUBY test.rb bovw512k_iv.yaml && \
+$RUBY test_similarity.rb bovw512k_nodb.yaml && \
+$RUBY test_similarity.rb vlad_nodb.yaml
